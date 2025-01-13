30f6a3259effd16d349b96299cb88e35d007012f
saurav0546 <saurav3249@gmail.com>
2025-01-13 11:39:37 +0530

    fix : optimize the code

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -20,17 +20,24 @@ class MedicineController extends Controller
     // Get medicines
     public function index(Request $request)
     {
-        // Pagination
-        $perPage = 7;
 
-        // Query the medicines
+        $perPage = 7;
         $medicines = Medicine::query();
 
         // Apply filters
-        $this->applyFilters($medicines, $request);
+        if ($request->has('ordered_by')) {
+            $medicines->searchByCustomerName($request->input('ordered_by'));
+        }
+        if ($request->has('price_min') || $request->has('price_max')) {
+            $medicines->priceRange($request->input('price_min'), $request->input('price_max'));
+        }
 
         // Apply sorting
-        $this->applySorting($medicines, $request);
+        if ($request->has('sort_by') && $request->has('sort_order')) {
+            $medicines->sortBy($request->input('sort_by'), $request->input('sort_order'));
+        } else {
+            $medicines->sortBy('name', 'asc');  // Default sorting
+        }
 
         // Pagination logic
         $medicines = $request->page ? $medicines->simplePaginate($perPage) : $medicines->get();
@@ -41,36 +48,6 @@ class MedicineController extends Controller
             'message' => __('messages.medicines.fetched'),
             'status' => '1'
         ]);
-    }
-
-    // Filters
-    private function applyFilters($query, Request $request)
-    {
-        // Filtering by customer
-        if ($request->has('ordered_by')) {
-            $search = $request->input('ordered_by');
-            $query->searchByCustomerName($search);  // Using the search scope
-        }
-
-        // Filtering by price
-        if ($request->has('price_min') || $request->has('price_max')) {
-            $minPrice = $request->input('price_min');
-            $maxPrice = $request->input('price_max');
-            $query->priceRange($minPrice, $maxPrice);  // Using the price range scope
-        }
-    }
-
-    // Sorting
-    private function applySorting($query, Request $request)
-    {
-        // Sorting
-        if ($request->has('sort_by') && $request->has('sort_order')) {
-            $sortBy = $request->input('sort_by');
-            $sortOrder = $request->input('sort_order');
-            $query->sortBy($sortBy, $sortOrder);  // Using sort scope
-        }
-        // Default sorting
-        $query->sortBy('name', 'asc');
     }
     // Create a medicine
     public function store(StoreMedicineRequest $request, Medicine $medicine)
@@ -116,12 +93,12 @@ class MedicineController extends Controller
         //     'data' => $medicines,
         //     'message' => __('messages.medicines.created')
         // ]);
-        
+
         $files = [];
 
         // single medicine creation
         if (!$request->has('data')) {
-           
+
             $medicine = Medicine::create([
                 'name' => $request->name,
                 'description' => $request->description,
@@ -131,13 +108,13 @@ class MedicineController extends Controller
 
             // Checking for files and store them
             if ($request->hasFile('files')) {
-                $files = []; 
+                $files = [];
 
                 foreach ($request->file('files') as $file) {
                     $filePath = $file->store('medicines/files', 'public');
 
-                    $fileUrl = Storage::url($filePath); 
-                    $files[] = $fileUrl; 
+                    $fileUrl = Storage::url($filePath);
+                    $files[] = $fileUrl;
                 }
 
                 // Save the file URLs/paths as JSON in the database
@@ -166,17 +143,19 @@ class MedicineController extends Controller
 
             if ($request->hasFile('files')) {
                 foreach ($request->file('files') as $file) {
-                    $filePath = $file->store('medicines/files',
+                    $filePath = $file->store(
+                        'medicines/files',
                         'public'
-                    ); 
+                    );
                     $files[] = $filePath;
                 }
                 $medicine->files = json_encode($files);
                 $medicine->save();
             }
             $medicines[] = $medicine;
-            
-            Storage::append('medicines.txt',
+
+            Storage::append(
+                'medicines.txt',
                 json_encode($medicineData)
             );
         }
@@ -185,14 +164,14 @@ class MedicineController extends Controller
             'data' => $medicines,
             'message' => __('messages.medicines.created')
         ], 201);
-    } 
-        
+    }
+
     // Show single medicine
     public function show(Medicine $medicine)
     {
         // Authorization
         $this->authorize('view', $medicine);
-        
+
         return response()->json([
             'data' => $medicine,
             'message' => __('messages.medicines.found')
@@ -202,7 +181,7 @@ class MedicineController extends Controller
     // Update medicine
     public function update(MedicineUpdateRequest $request, Medicine $medicine)
     {
-        
+
         if ($request->name) {
             $medicine->name = $request->name;
         }
@@ -232,7 +211,7 @@ class MedicineController extends Controller
     public function destroy(Medicine $medicine)
     {
         $this->authorize('delete', $medicine);
-        
+
         $medicine->delete();
         return response()->json([
             'data' => $medicine,
